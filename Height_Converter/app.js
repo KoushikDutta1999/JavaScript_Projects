93260805cbc15f82e434ef0beaa67188891135de
Koushik Dutta <78608176+KoushikDutta99@users.noreply.github.com>
2021-09-26 02:11:53 +0530

    first commit

---
+++ Height_Converter/app.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit',function(e)  {
+    
+    let feet = document.querySelector('#feet');
+    let inches = document.querySelector('#inches');
+    const results = document.querySelector('#results');
+    e.preventDefault();
+
+    feet = parseInt(feet.value);
+    inches = parseInt(inches.value);
+
+    if(isNaN(feet) || isNaN(inches)){
+        results.textContent = "Please enter your correct height!!!";
+    }
+    else if(feet < 0){
+        results.textContent = "Please enter your correct height!!!";
+    }
+    else if(inches < 0 || inches >= 12){
+        results.textContent = "Please enter your correct height!!!";
+    }
+    else{
+        let totalInches = (feet*30.48) + (inches*2.54);
+        results.textContent = `${totalInches} cm`;
+        document.querySelector('#feet').value = '';
+        document.querySelector('#inches').value = '';
+    }
+});

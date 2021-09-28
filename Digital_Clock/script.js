3279aa2e6098d03742492d137eb066078eb63f8e
“KoushikDutta99” <iamkoushik1999@gmail.com>
2021-09-28 11:31:32 +0530

    committed

--- Digital_Clock/script.js
+++ Digital_Clock/script.js
@@ -4,3 +4,14 @@ setInterval(function(){
     const date = new Date();
     clock.innerHTML = date.toLocaleTimeString();
 })
+
+/*function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+  }*/
